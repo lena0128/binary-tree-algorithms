4bd5b5d2672f354b33df50fb6284f87909590d4b
Lena Hou <lenahou2013@gmail.com>
2022-03-09 19:43:34 -0500

    breadth-first values

---
+++ breadthFirstValuesIteration.js
@@ -0,0 +1,20 @@
+
+// We consider the first node as the front one in the queue
+// Use the Array.shift() method to remove the first element from the array and return the removed element
+// 
+const breadthFirstValues = (root) => {
+    if (root === null) return [];
+
+    const result = [];
+    const queue = [ root ];
+    while (queue.length > 0) {
+        const current = queue.shift(); // remove the front node from the queue;
+        // as soon as the node leaves the queue, we can consider that as visited and add it to the result array
+        result.push(current.val); // add the value of the current node to the array;
+        
+        if (current.left !== null) queue.push(current.left) 
+        if (current.right !== null) queue.push(current.right)
+    }
+
+    return result;
+}
---
+++ breadthFirstValuesRecursion.js
@@ -0,0 +1,2 @@
+// There is not effective recursive solution to solve breadth-first values algorithm. Because it needs a queue order.
+// Recursion, under the hood, needs a stack.
